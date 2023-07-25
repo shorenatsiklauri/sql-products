f4fddd341df5da3a385b56c6753ebf48ae4a1328
shorenatsiklauri <Shorena.tsiklauri1985@gmail.com>
2023-07-25 18:04:59 +0400

    changerouter

--- src/routes/product.router.js
+++ src/routes/product.router.js
@@ -1,14 +1,14 @@
 import express from "express";
 import {
-  createProduct,
-  getAllproducts,
-  deleteProduct,
+  getAllTodos,
+  createTodo,
+  deleteTodo,
 } from "../controllers/prodacts.controler.js";
 
 const productRouter = express.Router();
 
-productRouter.get("/products", getAllproducts);
-productRouter.post("/products", createProduct);
-productRouter.delete("/products/:id", deleteProduct);
+productRouter.get("/products", getAllTodos);
+productRouter.post("/products", createTodo);
+productRouter.delete("/products/:id", deleteTodo);
 
 export default productRouter;
