cfe57a8489db481c5f2a7520642624377a16acb9
shorenatsiklauri <Shorena.tsiklauri1985@gmail.com>
2023-07-25 17:56:41 +0400

    todoapp

--- src/controllers/prodacts.controler.js
+++ src/controllers/prodacts.controler.js
@@ -1,8 +1,8 @@
 import pool from "../config/sql.js";
 
-export const getAllproducts = async (_, response) => {
+export const getAllTodos = async (_, response) => {
   try {
-    const resultQuery = await pool.query("SELECT * FROM products");
+    const resultQuery = await pool.query("SELECT * FROM todos");
     const rows = resultQuery.rows;
     return response.status(200).json(rows);
   } catch (error) {
@@ -10,13 +10,13 @@ export const getAllproducts = async (_, response) => {
   }
 };
 
-export const createProduct = async (req, res) => {
-  const { title, price } = req.body;
+export const createTodo = async (req, res) => {
+  const { text, completed } = req.body;
 
   try {
     const resultQuery = await pool.query(
-      "INSERT INTO  products(title,price) VALUES($1,$2)",
-      [title, price]
+      "INSERT INTO todos(text,completed) VALUES($1,$2)",
+      [text, completed]
     );
     const row = resultQuery.rows[0];
     return res.status(201).json(row);
@@ -25,15 +25,15 @@ export const createProduct = async (req, res) => {
   }
 };
 
-export const deleteProduct = async (req, res) => {
+export const deleteTodo = async (req, res) => {
   const id = +req.params.id;
 
   try {
-    await pool.query("DELETE FROM products WHERE id = $1", [id]);
-    return res.status(201).json({ message: "Product deleted" });
+    await pool.query("DELETE FROM todos WHERE id = $1", [id]);
+    return res.status(200).json({ message: "Todo deleted" });
   } catch (error) {
     return res
       .status(401)
-      .json({ error: "An error occurred while deleting the product" });
+      .json({ error: "An error occurred while deleting the todo" });
   }
 };
